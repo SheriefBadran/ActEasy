825344d9613434fbdc7e31eb7af8644a370175d6
SheriefBadran <sb222rf@student.lnu.se>
2015-01-15 19:25:25 +0100

    Refactoring: Middleware is gone and data is processed with the async npm module. It is now easier to control errors and data flow.

--- routes/activityRouts.js
+++ routes/activityRouts.js
@@ -2,6 +2,7 @@
  * Created by sheriefbadran on 1/3/15.
  */
 var request = require('request');
+var async = require('async');
 var mongoose = require('mongoose');
 var activitySchema = require('./../models/activity');
 var Activity = mongoose.model('Activity', activitySchema);
@@ -11,73 +12,94 @@ var evaluateActivities = require('./../priorityAlgorithm.js');
 var options = {};
 module.exports = function(app) {
 
-  app.use(function (req, res, next) {
-
-    if (req.param('lat') && req.param('lon')) {
-
-      var lat = +(Math.round(parseFloat(req.param('lat')) + "e+2") + "e-2");
-      var lon = +(Math.round(parseFloat(req.param('lon')) + "e+2") + "e-2");
-
-      options.url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/'+lat+'/lon/'+lon+'/data.json';
-      console.log(options.url);
-      options.headers = {
-        'User-Agent': 'Sherief Badran - student linneus university'
-      };
-
-      User.findOne({'google.name': 'Sherief Badran'}, function (err, user) {
-
-        if (user.nextupdate < +new Date()) {
-
-          // Request to SMHI API
-          request.get(options, function (error, response, body) {
-
-            // Maybe cache to file here.
-            var weather = JSON.parse(body);
-            user.weather = {
-              lat: weather.lat,
-              lon: weather.lon,
-              t: weather.timeseries[6].t,
-              ws: weather.timeseries[6].ws,
-              pit: weather.timeseries[6].pit,
-              pis: weather.timeseries[6].pis
-            };
-
-            user.nextupdate = +new Date() + 600000;
-
-            user.save();
-
-            next();
-          })
-        }
-        // Retrieve weather from database and assign to the request object
-        req.weather = user.weather;
-        next();
-      });
-    }
-    else {
-
-      next();
-    }
-  });
-
   // =====================================
   // ACTIVITIES ROUTE ====================
   // =====================================
   // MONGO DB geospatial query
   app.get('/near-activities', function (req, res) {
 
-    // (lat, long) is given in opposite order to mongoDB geospatial query, hence (long, lat)
-    Activity.find({
-      loc: {
-        $geoWithin: {
-          $centerSphere: [
-            [req.param('lon'), req.param('lat')], 500/6371]
-        }
-      }
-    }, function(err, activities) {
+    var options = {};
+    var lat = +(Math.round(parseFloat(req.param('lat')) + "e+2") + "e-2");
+    var lon = +(Math.round(parseFloat(req.param('lon')) + "e+2") + "e-2");
 
-      var evaluatedActivities = evaluateActivities(activities, req.weather);
-      console.log(evaluatedActivities);
+    async.parallel({
+      /*
+       * Request to weather API and connect weather to user.
+       */
+      weather: function(callback)
+      {
+
+        options.url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/' + lat + '/lon/' + lon + '/data.json';
+        options.headers = {
+          'User-Agent': 'Sherief Badran - student linneus university'
+        };
+
+        User.findOne({'google.name': 'Sherief Badran'}, function (err, user) {
+
+          // user.nextupdate
+          if (1 < +new Date()) {
+
+            request.get(options, function (error, response, body) {
+
+              if (response.statusCode === 200) {
+
+                console.log('200 OK');
+                // Maybe cache to file here.
+                var weather = JSON.parse(body);
+                user.weather = {
+                  lat: weather.lat,
+                  lon: weather.lon,
+                  t: weather.timeseries[6].t,
+                  ws: weather.timeseries[6].ws,
+                  pit: weather.timeseries[6].pit,
+                  pis: weather.timeseries[6].pis
+                };
+
+                user.nextupdate = +new Date() + 600000;
+                user.save();
+                callback(false, user.weather);
+              }
+            });
+          }
+          else {
+            // Retrieve weather from database and assign to the request object
+            callback(false, user.weather);
+          }
+        });
+      },
+      /*
+       * Fetch activities from server. GeoSpatial query
+       */
+      activities: function (callback) {
+
+        Activity.find({
+          loc: {
+            $geoWithin: {
+              $centerSphere: [
+                [req.param('lon'), req.param('lat')], 500 / 6371]
+            }
+          }
+        }, function (err, activities) {
+
+          console.log('activities sent with score');
+          //var evaluatedActivities = evaluateActivities(activities, req.weather);
+          callback(false, activities);
+        });
+      }
+    },
+    /*
+     * Combine results in ascyn.parallel callback (async.parallel([function(callback){}, function(callback){}], combineCallback(err, result){}))
+     */
+    function(err, result) {
+
+      if(err) {
+
+        console.log(err);
+        res.send(500,"Server Error");
+        return;
+      }
+
+      var evaluatedActivities = evaluateActivities(result.activities, result.weather);
       res.send(evaluatedActivities);
     });
   });
@@ -89,6 +111,4 @@ module.exports = function(app) {
       res.send(activity);
     });
   });
-
-
 };
